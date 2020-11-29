2fae816833178ee8ee2015ad61960365b50a194d
gowri666 <gowriarunn2008@gmail.com>
2020-11-29 15:36:12 +0530

    marking for the car

--- js/Game.js
+++ js/Game.js
@@ -66,7 +66,10 @@ class Game {
         cars[index-1].x = x;
         cars[index-1].y = y;
 
+
         if (index === player.index){
+          fill("yellow");
+          ellipse(x,y,60,60)
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
